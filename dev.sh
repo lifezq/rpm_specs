393bd4b3c73bc2e8ea3a8285857bdf055ea8b326
Ryan <lifezqy@126.com>
2017-06-09 17:46:14 +0800

    devsh

--- dev.sh
+++ dev.sh
@@ -5,7 +5,7 @@
 #
 
 # Yum repo
-yum install -y epel-release gcc gcc-c++ make cpp glibc-headers kernel-headers automake autoconf openssl-devel pcre-devel readline-devel libcurl-devel wget net-tools psmisc
+yum install -y epel-release gcc gcc-c++ make cpp glibc-headers kernel-headers automake autoconf openssl-devel pcre-devel readline-devel libcurl-devel wget net-tools
 wget https://raw.githubusercontent.com/lifezq/rpm_specs/617abbe2653f2e35ffd70f34b307f05317b8fd83/repo/ryan.repo -O /etc/yum.repos.d/ryan.repo
 yum clean all && yum makecache fast
 
@@ -18,8 +18,30 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 
 # Monitor Commands
 yum install -y dstat
-# dstat -cdlmnpsyr --socket --tcp
+# usage: dstat -cdlmnpsyr --socket --tcp
 
 # Backup 
 # rsync
-# rdiff-backup
+# rdiff-backup 
+
+# Port scan
+yum install -y nmap
+# usage: nmap -sS 192.168.0.1/24
+
+# trace route
+yum install -y traceroute mtr
+
+# system profile
+yum install -y sysstat psmisc
+
+# disk profile
+yum install -y hdparm
+# usage: hdparm -I /dev/sda 
+
+# net profile
+yum install -y tcpdump
+# usage: tcpdump -i enp5s0 -vv host domain
+
+# pci
+# yum install -y lspci
+# usage: lspci -vvv | grep Ethernet
