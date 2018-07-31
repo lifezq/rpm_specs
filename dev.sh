82683ff4145a51a8caf32ba2896089f17b9356d1
Ryan <lifezq@users.noreply.github.com>
2018-07-31 11:17:41 +0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -6,7 +6,7 @@
 
 # Yum repo
 yum install -y epel-release gcc gcc-c++ make cpp glibc-headers kernel-headers automake autoconf openssl-devel pcre-devel readline-devel libcurl-devel wget net-tools
-wget https://raw.githubusercontent.com/lifezq/rpm_specs/617abbe2653f2e35ffd70f34b307f05317b8fd83/repo/ryan.repo -O /etc/yum.repos.d/ryan.repo
+wget https://raw.githubusercontent.com/lifezq/rpm_specs/master/repo/ryan.repo -O /etc/yum.repos.d/ryan.repo
 yum clean all && yum makecache fast
 
 # Desktop
