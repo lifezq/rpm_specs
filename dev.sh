53aee0e2008ffaa399e718f0945013d872a17fb2
Ryan <lifezq@users.noreply.github.com>
2018-08-16 23:52:27 +0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -48,3 +48,5 @@ yum install -y tcpdump
 
 # /etc/profile
 # export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+# echo "192.30.253.112 github.com\n151.101.13.194 github.global.ssl.fastly.net\n52.216.65.8 github-cloud.s3.amazonaws.com" >> /etc/hosts
