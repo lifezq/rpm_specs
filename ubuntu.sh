0bea3eea2df163b574ddce8deb9faeca097376c0
Ryan <lifezq@users.noreply.github.com>
2019-08-30 20:26:39 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,19 +3,11 @@
 # ubuntu change source
 tee /etc/apt/sources.list <<-'EOF'
 deb http://mirrors.163.com/debian/ jessie main non-free contrib
-
 deb http://mirrors.163.com/debian/ jessie-updates main non-free contrib
-
 deb http://mirrors.163.com/debian/ jessie-backports main non-free contrib
-
 deb-src http://mirrors.163.com/debian/ jessie main non-free contrib
-
 deb-src http://mirrors.163.com/debian/ jessie-updates main non-free contrib
-
 deb-src http://mirrors.163.com/debian/ jessie-backports main non-free contrib
-
 deb http://mirrors.163.com/debian-security/ jessie/updates main non-free contrib
-
 deb-src http://mirrors.163.com/debian-security/ jessie/updates main non-free contrib
-
 EOF
