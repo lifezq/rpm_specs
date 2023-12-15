5aceb8074086e06a5ea75606bea4e8bcb8a8fa3f
Ryan <lifezq@users.noreply.github.com>
2023-12-15 11:56:12 +0800

    Update docker_service_boot.sh

--- docker_service_boot.sh
+++ docker_service_boot.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 ## rocket mq
 docker run --name mqnamesrv -p 9876:9876 -it apache/rocketmq ./mqnamesrv
-docker run --name mqbroker -e "JAVA_OPT_EXT=-Xmx128m -Xms128m" -p 10911:10911 -it apache/rocketmq ./mqbroker -n 192.168.0.103:9876
-docker run -d --name rocketmq-dashboard -e "JAVA_OPTS=-Drocketmq.namesrv.addr=192.168.0.103:9876" -p 8080:8080 -t apacherocketmq/rocketmq-dashboard:latest
+## 生成broker.conf
+cat >>broker.conf <<EOF
+brokerClusterName = DefaultCluster
+brokerName = broker-a
+brokerId = 0
+deleteWhen = 04
+fileReservedTime = 48
+brokerRole = ASYNC_MASTER
+flushDiskType = ASYNC_FLUSH
+brokerIP1 = 192.168.0.252
+EOF
+docker run --name mqbroker -e "JAVA_OPT_EXT=-Xmx128m -Xms128m" -p 10911:10911 -p 10909:10909 -v .\broker.conf:/opt/broker.conf -it apache/rocketmq ./mqbroker -n 192.168.0.252:9876 -c /opt/broker.conf
+docker run -d --name rocketmq-dashboard -e "JAVA_OPTS=-Drocketmq.namesrv.addr=192.168.0.252:9876" -p 8080:8080 -t apacherocketmq/rocketmq-dashboard:latest
 
 ## redis
 docker run -itd --name redis -p 6379:6379 redis:5.0.14-alpine --requirepass 123456
